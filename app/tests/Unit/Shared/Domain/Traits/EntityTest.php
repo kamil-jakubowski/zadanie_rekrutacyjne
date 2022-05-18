46b816382045b404d4c53839d0e7946b3eab4fc2
Kamil Jakubowski <kamil.jakubowski@gmail.com>
2022-05-18 22:55:00 +0200

    Correction of EntityTest

--- app/tests/Unit/Shared/Domain/Traits/EntityTest.php
+++ app/tests/Unit/Shared/Domain/Traits/EntityTest.php
@@ -1,11 +1,18 @@
 <?php
 declare(strict_types=1);
 
-use App\Shared\Domain\Traits\Entity;
+namespace App\Tests\Unit\Shared\Domain\Traits;
+
 use PHPUnit\Framework\TestCase;
 
 class EntityTest extends TestCase
 {
     //not really anything to test, only private/protected methods
     //this trait should be tested and at every Entity  that uses it in its test-class
+
+    public function testNothing(): void
+    {
+        $this->assertTrue(true);
+        // dumb, but to keep remember to test it at every Entity
+    }
 }
